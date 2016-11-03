cd62b266777b6c094c24d93212d43eff2eb4d43d
yhkim0426 <replaysite9@gmail.com>
2016-11-03 21:42:09 +0900

    add DataCenter.class, Prefix.pch

---
+++ 1vs100QuizGame/1vs100QuizGame/DataCenter.h
@@ -0,0 +1,13 @@
+//
+//  DataCenter.h
+//  1vs100QuizGame
+//
+//  Created by Nexus_MYT on 2016. 11. 3..
+//  Copyright © 2016년 진호놀이터. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataCenter : NSObject
+
+@end
