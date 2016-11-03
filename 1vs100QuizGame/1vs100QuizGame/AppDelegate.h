a640dfced636ece6a86c9c1b1d059e24639290c1
진호놀이터 <jinhos@jinhonol-iteoui-MacBook-Pro.local>
2016-11-03 21:33:46 +0900

    초기화면

---
+++ 1vs100QuizGame/1vs100QuizGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1vs100QuizGame
+//
+//  Created by 진호놀이터 on 2016. 11. 3..
+//  Copyright © 2016년 진호놀이터. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1vs100QuizGame/1vs100QuizGame/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  1vs100QuizGame
+//
+//  Created by 진호놀이터 on 2016. 11. 3..
+//  Copyright © 2016년 진호놀이터. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    self.tabBarController.tabBar.hidden = YES;
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
